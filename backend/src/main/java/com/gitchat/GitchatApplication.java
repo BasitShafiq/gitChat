14a6e276b296364ec9555f188b014adfc2f2281e
BasitShafiq <mughalabdul512@gmail.com>
2026-09-05 15:49:24 +0500

    initial

---
+++ backend/src/main/java/com/gitchat/GitchatApplication.java
@@ -0,0 +1,13 @@
+package com.gitchat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GitchatApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GitchatApplication.class, args);
+	}
+
+}
---
+++ backend/src/test/java/com/gitchat/GitchatApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gitchat;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitchatApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
